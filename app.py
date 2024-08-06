69cc4f0bc128c9b5561ec4713af835f1e36bd389
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2024-08-06 19:37:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from bs4 import BeautifulSoup
 import requests
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/get-link', methods=['GET'])
-def get_link():
+@app.get("/get-link")
+async def get_link():
     url = "https://bingotingo.com/best-social-media-platforms/"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     link_element = soup.find('a', class_='su-button su-button-s')
 
     if link_element and link_element.has_attr('href'):
-        return jsonify({'link': link_element['href']})
-    return jsonify({'link': None})
+        return JSONResponse(content={'link': link_element['href']})
+    return JSONResponse(content={'link': None})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
