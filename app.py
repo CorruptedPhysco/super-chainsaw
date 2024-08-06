d32fb2c0115ae3b2b74ea65c9421aa67ec054a00
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2024-08-06 21:18:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
 import requests
@@ -28,4 +29,5 @@ def home():
         return jsonify({"error": "Link not found"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
